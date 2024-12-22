2028c89520c5daa4e7c879ff67deeb83420445e7
Donavan Becker <beckersmarthome@icloud.com>
2024-12-21 19:08:57 -0600

    Update switchbot-openapi.ts

--- src/switchbot-openapi.ts
+++ src/switchbot-openapi.ts
@@ -170,7 +170,7 @@ export class SwitchBotOpenAPI extends EventEmitter {
    * - `t`: The current timestamp in milliseconds since the Unix epoch.
    * - `Content-Type`: The content type of the request, set to `application/json`.
    */
-  private generateHeaders = (): { 'Authorization': string, 'sign': string, 'nonce': `${string}-${string}-${string}-${string}-${string}`, 't': string, 'Content-Type': string } => {
+  private generateHeaders = (): { 'Authorization': string, 'sign': string, 'nonce': `${string}-${string}-${string}-${string}-${string}`, 't': string, 'Content-Type': string, 'X-Amz-Date': string } => {
     const t = `${Date.now()}`
     const nonce = randomUUID()
     const data = this.token + t + nonce
@@ -179,13 +179,15 @@ export class SwitchBotOpenAPI extends EventEmitter {
       .update(Buffer.from(data, 'utf-8'))
       .digest()
     const sign = signTerm.toString('base64')
+    const amzDate = new Date().toISOString().replace(/[:-]|\.\d{3}/g, '')
 
     return {
-      'Authorization': this.token,
+      'Authorization': `Credential=${this.token}, Signature=${sign}, SignedHeaders=content-type;host;x-amz-date, SignedHeaders=content-type;host;x-amz-date`,
       'sign': sign,
       'nonce': nonce,
       't': t,
       'Content-Type': 'application/json',
+      'X-Amz-Date': amzDate,
     }
   }
 
